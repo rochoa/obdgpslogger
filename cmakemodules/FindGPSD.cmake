d53203ea3386bd8ac9b96b1c7af87a5b9c75860d
chunky <chunky@ec62d176-4a65-0410-9229-e4683bb12484>
2009-03-24 17:22:44 +0000

    Rename obdlogger to obdgpslogger Add a changelog CMake sniglets to make it all more friendly to fink
    git-svn-id: svn://svn.icculus.org/obdgpslogger/trunk@24 ec62d176-4a65-0410-9229-e4683bb12484

--- cmakemodules/FindGPSD.cmake
+++ cmakemodules/FindGPSD.cmake
@@ -5,6 +5,7 @@ set(CHUNKY_INSTALLED_GPSD_ON_HIS_MAC_IN $ENV{HOME}/gpsd/)
 SET(GPSD_PREFIX "/usr/local/gpsd/" CACHE STRING "GPSD is installed in this prefix (if non-standard)")
 
 find_path(GPSD_INCLUDE_DIR gps.h
+	/sw/include/
 	/usr/include/
 	/usr/local/include/
 	${GPSD_PREFIX}/include/
@@ -12,6 +13,7 @@ find_path(GPSD_INCLUDE_DIR gps.h
 )
 
 find_library(GPSD_LIBRARY NAMES gps PATHS
+	/sw/lib/
 	/usr/lib
 	/usr/local/lib
 	${GPSD_PREFIX}/lib
