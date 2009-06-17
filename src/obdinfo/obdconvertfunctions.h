5d0d9037cd623b14f2ee5fdc82d882f85d1f2f8d
chunky <chunky@ec62d176-4a65-0410-9229-e4683bb12484>
2009-06-17 02:06:28 +0000

    Add reverse convert functions
    git-svn-id: svn://svn.icculus.org/obdgpslogger/trunk@128 ec62d176-4a65-0410-9229-e4683bb12484

--- src/obdinfo/obdconvertfunctions.h
+++ src/obdinfo/obdconvertfunctions.h
@@ -18,6 +18,11 @@ extern "C" {
 typedef float (*OBDConvFunc)(unsigned int A, unsigned int B,
 	unsigned int C, unsigned int D);
 
+/// All obd reverse conversion functions adhere to this
+/// return value is number of values [A,B,C,D] filled.
+typedef int (*OBDConvRevFunc)(float val, unsigned int *A, unsigned int *B,
+	unsigned int *C, unsigned int *D);
+
 float obdConvert_04    (unsigned int A, unsigned int B, unsigned int C, unsigned int D);
 float obdConvert_05    (unsigned int A, unsigned int B, unsigned int C, unsigned int D);
 float obdConvert_06_09 (unsigned int A, unsigned int B, unsigned int C, unsigned int D);
@@ -56,6 +61,43 @@ float obdConvert_4D    (unsigned int A, unsigned int B, unsigned int C, unsigned
 float obdConvert_4E    (unsigned int A, unsigned int B, unsigned int C, unsigned int D);
 float obdConvert_52    (unsigned int A, unsigned int B, unsigned int C, unsigned int D);
 
+int obdRevConvert_04    (float val, unsigned int *A, unsigned int *B, unsigned int *C, unsigned int *D);
+int obdRevConvert_05    (float val, unsigned int *A, unsigned int *B, unsigned int *C, unsigned int *D);
+int obdRevConvert_06_09 (float val, unsigned int *A, unsigned int *B, unsigned int *C, unsigned int *D);
+int obdRevConvert_0A    (float val, unsigned int *A, unsigned int *B, unsigned int *C, unsigned int *D);
+int obdRevConvert_0B    (float val, unsigned int *A, unsigned int *B, unsigned int *C, unsigned int *D);
+int obdRevConvert_0C    (float val, unsigned int *A, unsigned int *B, unsigned int *C, unsigned int *D);
+int obdRevConvert_0D    (float val, unsigned int *A, unsigned int *B, unsigned int *C, unsigned int *D);
+int obdRevConvert_0E    (float val, unsigned int *A, unsigned int *B, unsigned int *C, unsigned int *D);
+int obdRevConvert_0F    (float val, unsigned int *A, unsigned int *B, unsigned int *C, unsigned int *D);
+int obdRevConvert_10    (float val, unsigned int *A, unsigned int *B, unsigned int *C, unsigned int *D);
+int obdRevConvert_11    (float val, unsigned int *A, unsigned int *B, unsigned int *C, unsigned int *D);
+int obdRevConvert_14_1B (float val, unsigned int *A, unsigned int *B, unsigned int *C, unsigned int *D);
+int obdRevConvert_1F    (float val, unsigned int *A, unsigned int *B, unsigned int *C, unsigned int *D);
+int obdRevConvert_21    (float val, unsigned int *A, unsigned int *B, unsigned int *C, unsigned int *D);
+int obdRevConvert_22    (float val, unsigned int *A, unsigned int *B, unsigned int *C, unsigned int *D);
+int obdRevConvert_23    (float val, unsigned int *A, unsigned int *B, unsigned int *C, unsigned int *D);
+int obdRevConvert_24_2B (float val, unsigned int *A, unsigned int *B, unsigned int *C, unsigned int *D);
+int obdRevConvert_2C    (float val, unsigned int *A, unsigned int *B, unsigned int *C, unsigned int *D);
+int obdRevConvert_2D    (float val, unsigned int *A, unsigned int *B, unsigned int *C, unsigned int *D);
+int obdRevConvert_2E    (float val, unsigned int *A, unsigned int *B, unsigned int *C, unsigned int *D);
+int obdRevConvert_2F    (float val, unsigned int *A, unsigned int *B, unsigned int *C, unsigned int *D);
+int obdRevConvert_30    (float val, unsigned int *A, unsigned int *B, unsigned int *C, unsigned int *D);
+int obdRevConvert_31    (float val, unsigned int *A, unsigned int *B, unsigned int *C, unsigned int *D);
+int obdRevConvert_32    (float val, unsigned int *A, unsigned int *B, unsigned int *C, unsigned int *D);
+int obdRevConvert_33    (float val, unsigned int *A, unsigned int *B, unsigned int *C, unsigned int *D);
+int obdRevConvert_34_3B (float val, unsigned int *A, unsigned int *B, unsigned int *C, unsigned int *D);
+int obdRevConvert_3C_3F (float val, unsigned int *A, unsigned int *B, unsigned int *C, unsigned int *D);
+int obdRevConvert_42    (float val, unsigned int *A, unsigned int *B, unsigned int *C, unsigned int *D);
+int obdRevConvert_43    (float val, unsigned int *A, unsigned int *B, unsigned int *C, unsigned int *D);
+int obdRevConvert_44    (float val, unsigned int *A, unsigned int *B, unsigned int *C, unsigned int *D);
+int obdRevConvert_45    (float val, unsigned int *A, unsigned int *B, unsigned int *C, unsigned int *D);
+int obdRevConvert_46    (float val, unsigned int *A, unsigned int *B, unsigned int *C, unsigned int *D);
+int obdRevConvert_47_4B (float val, unsigned int *A, unsigned int *B, unsigned int *C, unsigned int *D);
+int obdRevConvert_4C    (float val, unsigned int *A, unsigned int *B, unsigned int *C, unsigned int *D);
+int obdRevConvert_4D    (float val, unsigned int *A, unsigned int *B, unsigned int *C, unsigned int *D);
+int obdRevConvert_4E    (float val, unsigned int *A, unsigned int *B, unsigned int *C, unsigned int *D);
+int obdRevConvert_52    (float val, unsigned int *A, unsigned int *B, unsigned int *C, unsigned int *D);
 
 #ifdef __cplusplus
 }
