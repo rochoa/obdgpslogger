8899596453d3065c4f69420b57c4b7d4e9284f84
chunky <chunky@ec62d176-4a65-0410-9229-e4683bb12484>
2009-06-29 06:08:21 +0000

    Initial add of [non-functional] fltk gui generator plugin
    git-svn-id: svn://svn.icculus.org/obdgpslogger/trunk@179 ec62d176-4a65-0410-9229-e4683bb12484

---
+++ src/sim/generators/gui_fltk/gen_gui_fltk.c
@@ -0,0 +1,16 @@
+/** \file
+ \brief Generate data from FLTK GUI
+*/
+
+/* Do not bother trying to compile this. It's just going to be
+    #included in another C file. It's here to work around a limitation
+    in fluid. */
+
+struct obdsim_generator obdsimgen_gui_fltk = {
+	guifltk_simgen_name,
+	guifltk_simgen_create,
+	guifltk_simgen_destroy,
+	guifltk_simgen_getvalue,
+	guifltk_simgen_idle
+};
+
