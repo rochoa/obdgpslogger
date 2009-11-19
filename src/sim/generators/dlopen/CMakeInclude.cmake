4e7b768d75f3e2e6db9cdcc9b413b886a455cba8
chunky <chunky@ec62d176-4a65-0410-9229-e4683bb12484>
2009-11-19 07:57:55 +0000

    Tidy up checks for dlopen
    git-svn-id: svn://svn.icculus.org/obdgpslogger/trunk@289 ec62d176-4a65-0410-9229-e4683bb12484

--- src/sim/generators/dlopen/CMakeInclude.cmake
+++ src/sim/generators/dlopen/CMakeInclude.cmake
@@ -1,9 +1,18 @@
 SET(OBD_SIMGEN_DLOPEN "On" CACHE BOOL "Enable dlopen obdsim generator")
 
 INCLUDE(CheckFunctionExists)
+SET(CMAKE_REQUIRED_LIBRARIES dl)
 CHECK_FUNCTION_EXISTS(dlopen HAVE_DLOPEN)
+UNSET(CMAKE_REQUIRED_LIBRARIES)
 
-IF(OBD_SIMGEN_DLOPEN AND HAVE_DLOPEN)
+IF(NOT HAVE_DLOPEN)
+	SET(OBD_SIMGEN_DLOPEN "Off" CACHE BOOL "Enable dlopen obdsim generator" FORCE)
+	MESSAGE(STATUS "Couldn't find dlopen. Not building dlopen sim generator")
+ELSE(NOT HAVE_DLOPEN)
+	MESSAGE(STATUS "Found dlopen. Building dlopen sim generator")
+ENDIF(NOT HAVE_DLOPEN)
+
+IF(OBD_SIMGEN_DLOPEN)
 	FILE(GLOB OBDSIMGEN_DLOPEN_SRCS
 		generators/dlopen/*.c
 	)
@@ -15,5 +24,5 @@ IF(OBD_SIMGEN_DLOPEN AND HAVE_DLOPEN)
 
 # This is just the test library
 	ADD_LIBRARY(obdsim_dltest MODULE generators/dlopen/test/test_dlopen.c)
-ENDIF(OBD_SIMGEN_DLOPEN AND HAVE_DLOPEN)
+ENDIF(OBD_SIMGEN_DLOPEN)
 
