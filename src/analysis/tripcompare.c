3daefaa9d4ae8e73159c75349ad7dc928e65e004
chunky <chunky@ec62d176-4a65-0410-9229-e4683bb12484>
2010-02-06 06:54:04 +0000

    Initial estimate of miles per gallon for each trip
    git-svn-id: svn://svn.icculus.org/obdgpslogger/trunk@378 ec62d176-4a65-0410-9229-e4683bb12484

--- src/analysis/tripcompare.c
+++ src/analysis/tripcompare.c
@@ -15,6 +15,12 @@ void printhelp(const char *argv0);
  */
 int comparetrips(sqlite3 *db, int tripA, int tripB, int gpspointsA, int gpspointsB);
 
+/// How much petrol do we think was drunk this trip?
+double thirstyboy(sqlite3 *db, int trip);
+
+/// Total length of this trip
+double tripdist(sqlite3 *db, int trip);
+
 int main(int argc, char *argv[]) {
 	// Do not attempt to buffer stdout
 	setvbuf(stdout, (char *)NULL, _IONBF, 0);
@@ -34,6 +40,27 @@ int main(int argc, char *argv[]) {
 		sqlite3_close(db);
 		exit(1);
 	}
+
+	// Find out how much petrol each trip burned
+
+	sqlite3_stmt *obdtripstmt;
+	const char obdtrip_sql[] = "SELECT DISTINCT trip FROM obd ORDER BY TRIP";
+
+	rc = sqlite3_prepare_v2(db, obdtrip_sql, -1, &obdtripstmt, NULL);
+	if(SQLITE_OK != rc) {
+		fprintf(stderr, "Cannot prepare select statement gpstrips (%i): %s\n", rc, sqlite3_errmsg(db));
+		sqlite3_close(db);
+		exit(1);
+	}
+
+	while(SQLITE_ROW == sqlite3_step(obdtripstmt)) {
+		thirstyboy(db, sqlite3_column_int(obdtripstmt, 0));
+	}
+
+	sqlite3_finalize(obdtripstmt);
+
+
+	// See which trips are actually the same
 
 	// Need a pair of trip statements for iterating
 	const char tripselect_sql[] = "SELECT trip,COUNT(lat) FROM gps "
@@ -79,6 +106,28 @@ int main(int argc, char *argv[]) {
 	sqlite3_finalize(tripstmt_B);
 	sqlite3_close(db);
 	return 0;
+}
+
+double haversine_dist(double latA, double lonA, double latB, double lonB) {
+	// Haversine formula
+	// R = earth radius ~= 6,371km
+	// delta lat = lat2 − lat1
+	// delta lon = lon2 − lon1
+	// a = sin2(delta lat/2) + cos(lat1) * cos(lat2) * sin2(delta lon/2)
+	// c = 2 * atan2(sqrt(a), sqrt(1−a))
+	// d = R * c
+
+	double R = 6371;
+	double dlat = latB-latA;
+	double dlon = lonB-lonA;
+	double sinlat = sin((dlat/2) * (M_PI/180));
+	double sinlon = sin((dlon/2) * (M_PI/180));
+
+	double a=sinlat*sinlat + sinlon*sinlon * cos(latA * (M_PI/180)) * cos(latB * (M_PI/180));
+	double c = 2 * atan2(sqrt(a), sqrt(1-a));
+	double d = R*c;
+
+	return d;
 }
 
 int comparetrips(sqlite3 *db, int tripA, int tripB, int gpspointsA, int gpspointsB) {
@@ -135,24 +184,7 @@ int comparetrips(sqlite3 *db, int tripA, int tripB, int gpspointsA, int gpspoint
 			latB = sqlite3_column_double(gpsstmt_B, 0);
 			lonB = sqlite3_column_double(gpsstmt_B, 1);
 
-			// Haversine formula
-			// R = earth radius ~= 6,371km
-			// delta lat = lat2 − lat1
-			// delta lon = lon2 − lon1
-			// a = sin2(delta lat/2) + cos(lat1) * cos(lat2) * sin2(delta lon/2)
-			// c = 2 * atan2(sqrt(a), sqrt(1−a))
-			// d = R * c
-
-			double R = 6371;
-			double dlat = latB-latA;
-			double dlon = lonB-lonA;
-			double sinlat = sin((dlat/2) * (M_PI/180));
-			double sinlon = sin((dlon/2) * (M_PI/180));
-
-			double a=sinlat*sinlat + sinlon*sinlon * cos(latA * (M_PI/180)) * cos(latB * (M_PI/180));
-			double c = 2 * atan2(sqrt(a), sqrt(1-a));
-			double d = R*c;
-
+			double d=haversine_dist(latA, lonA, latB, lonB);
 			if(d < mind) mind = d;
 
 			if(OBDTRIP_CUTOFF > mind) {
@@ -181,6 +213,91 @@ int comparetrips(sqlite3 *db, int tripA, int tripB, int gpspointsA, int gpspoint
 	sqlite3_finalize(gpsstmt_B);
 
 	return retvalue;
+}
+
+double thirstyboy(sqlite3 *db, int trip) {
+	int rc;
+
+	int retvalue = 0;
+
+	const char mafselect_sql[] = "SELECT SUM(a.maf*(a.time-b.time)), count(a.maf), max(a.time) - min(a.time) "
+			"FROM obd a LEFT JOIN obd b "
+			"ON a.rowid=b.rowid+1 "
+			"WHERE a.trip=? AND b.trip=a.trip";
+
+	sqlite3_stmt *mafstmt;
+
+	rc = sqlite3_prepare_v2(db, mafselect_sql, -1, &mafstmt, NULL);
+	if(SQLITE_OK != rc) {
+		fprintf(stderr, "Cannot prepare select statement maf (%i): %s\n", rc, sqlite3_errmsg(db));
+		sqlite3_close(db);
+		return -1;
+	}
+
+	sqlite3_bind_int(mafstmt, 1, trip);
+	printf("Trip %i ", trip);
+
+	double trip_dist = tripdist(db, trip);
+	double total_maf;
+	double delta_time;
+	int maf_count;
+	if(SQLITE_ROW == sqlite3_step(mafstmt)) {
+		total_maf = sqlite3_column_double(mafstmt,0);
+		maf_count = sqlite3_column_int(mafstmt,1);
+		delta_time = sqlite3_column_double(mafstmt,2);
+	}
+
+	const float ratio = 0.147;
+	const float petrol_density = 737.22; //  kg/m^3
+	const float cubic_meters_to_gallons = 264.172052;
+	const float km_to_miles = 0.621371192;
+
+	double petrol_kilograms = ratio * total_maf / 1000.0;
+	double petrol_m3 = petrol_kilograms/petrol_density;
+	double petrol_gal = petrol_m3 * cubic_meters_to_gallons;
+
+	double trip_miles = trip_dist * km_to_miles;
+
+	printf("(%.0f sec): %.2fg petrol, %.3fgal, %.2fmiles, %.1fmpg\n",
+				delta_time, 1000*petrol_kilograms,
+				petrol_gal, trip_miles, trip_miles/petrol_gal);
+
+	sqlite3_finalize(mafstmt);
+
+	return total_maf;
+}
+
+double tripdist(sqlite3 *db, int trip) {
+	int rc;
+
+	double total_dst = 0;
+
+	const char dstselect_sql[] = "SELECT a.lat,a.lon,b.lat,b.lon "
+			"FROM gps a LEFT JOIN gps b "
+			"ON b.rowid=a.rowid+1 "
+			"WHERE a.trip=? AND b.trip=a.trip";
+
+	sqlite3_stmt *dststmt;
+
+	rc = sqlite3_prepare_v2(db, dstselect_sql, -1, &dststmt, NULL);
+	if(SQLITE_OK != rc) {
+		fprintf(stderr, "Cannot prepare select statement dst (%i): %s\n", rc, sqlite3_errmsg(db));
+		sqlite3_close(db);
+		return -1;
+	}
+
+	sqlite3_bind_int(dststmt, 1, trip);
+
+	while(SQLITE_ROW == sqlite3_step(dststmt)) {
+		total_dst += haversine_dist( sqlite3_column_double(dststmt, 0),
+							sqlite3_column_double(dststmt, 1),
+							sqlite3_column_double(dststmt, 2),
+							sqlite3_column_double(dststmt, 3) );
+	}
+
+	sqlite3_finalize(dststmt);
+
+	return total_dst;
 }
 
 void printhelp(const char *argv0) {
