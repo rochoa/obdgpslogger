546aebcf99554996bf50a23e866bc4fe5e58eb29
chunky <chunky@ec62d176-4a65-0410-9229-e4683bb12484>
2010-03-03 07:12:08 +0000

    obdfft, show ranges for each axis obdfft, fix csv parsing
    git-svn-id: svn://svn.icculus.org/obdgpslogger/trunk@402 ec62d176-4a65-0410-9229-e4683bb12484

--- src/analysis/fftmain.cpp
+++ src/analysis/fftmain.cpp
@@ -3,6 +3,17 @@
 
 int main(int argc, char **argv) {
 	FFTOBD mainwindow;
+
+	if(argc > 1) {
+		if(NULL != strstr(argv[1], ".csv")) {
+			mainwindow.opencsv(argv[1]);
+		} else if(NULL != strstr(argv[1], ".db")) {
+			mainwindow.opendb(argv[1]);
+		} else {
+			fprintf(stderr, "First argument to %s must be csv or db\n", argv[0]);
+		}
+	}
+
 	mainwindow.show(argc,argv);
 
 	Fl::run();
