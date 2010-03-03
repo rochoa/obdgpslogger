5a376583704251dd4e52eea1cc75f9219af12a36
chunky <chunky@ec62d176-4a65-0410-9229-e4683bb12484>
2010-03-03 07:15:36 +0000

    obdfft, accept .txt on commandline [treat as .csv] obdfft, allow greater nonlinear granularity values
    git-svn-id: svn://svn.icculus.org/obdgpslogger/trunk@403 ec62d176-4a65-0410-9229-e4683bb12484

--- src/analysis/fftmain.cpp
+++ src/analysis/fftmain.cpp
@@ -5,12 +5,12 @@ int main(int argc, char **argv) {
 	FFTOBD mainwindow;
 
 	if(argc > 1) {
-		if(NULL != strstr(argv[1], ".csv")) {
+		if(NULL != strstr(argv[1], ".csv") || NULL != strstr(argv[1], ".txt")) {
 			mainwindow.opencsv(argv[1]);
 		} else if(NULL != strstr(argv[1], ".db")) {
 			mainwindow.opendb(argv[1]);
 		} else {
-			fprintf(stderr, "First argument to %s must be csv or db\n", argv[0]);
+			fprintf(stderr, "First argument to %s must be csv,txt or db\n", argv[0]);
 		}
 	}
 
