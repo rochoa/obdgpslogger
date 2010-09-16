bcb0806f2eb9bf29e3162c71cdb00a62857b1290
chunky <chunky@ec62d176-4a65-0410-9229-e4683bb12484>
2010-09-16 03:40:03 +0000

    Solved one TODO Add search path to mingw build for libraries like fltk and fftw
    git-svn-id: svn://svn.icculus.org/obdgpslogger/trunk@519 ec62d176-4a65-0410-9229-e4683bb12484

--- cmakemodules/Toolchain-mingw32.cmake
+++ cmakemodules/Toolchain-mingw32.cmake
@@ -19,7 +19,7 @@ SET(CMAKE_C_COMPILER i586-mingw32msvc-gcc)
 SET(CMAKE_CXX_COMPILER i586-mingw32msvc-g++)
 
 # here is the target environment located
-SET(CMAKE_FIND_ROOT_PATH  /usr/i586-mingw32msvc )
+SET(CMAKE_FIND_ROOT_PATH  /usr/i586-mingw32msvc /home/chunky/mingwlibs )
 
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search 
